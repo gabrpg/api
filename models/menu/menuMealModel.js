51d2a3a8da618e5b7aec018fb35eec8d62089d12
gabriel dufresne <gabrielduf@hotmail.com>
2023-03-06 14:15:57 -0500

    correction du modele meal

--- models/menu/menuMealModel.js
+++ models/menu/menuMealModel.js
@@ -6,8 +6,6 @@ const menuMeal = new mongoose.Schema(
         menuMealPrice: { type: Number, required: true },
         menuMealDescription: { type: String, required: true },
         menuMealImage: { type: String, required: true },
-        menuMealRating: { type: Number, required: false, min: 0 },
-        menuMealTotalRating: {type: Number, required: true,min:0, default: 0},
         menuMealAllergens: [{type: ObjectId, ref: 'MenuAllergenModel'}],
         menuMealCategories: [{type: ObjectId, ref: 'MenuCategoryModel'}],
     }
