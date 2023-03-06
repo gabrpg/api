86d3a3ee088b971bd52f7e0752b383bbc23d0d90
gabriel dufresne <gabrielduf@hotmail.com>
2023-03-06 15:39:02 -0500

    correction d'erreur avec le model menuMealModel

--- models/ratingModel.js
+++ models/ratingModel.js
@@ -3,8 +3,8 @@ const ObjectId = mongoose.Schema.Types.ObjectId;
 const ratingMeal = new mongoose.Schema(
     {
         MealRating: { type: Number, required: false, min: 0 },
-        menuMeal: [{type: ObjectId, ref: 'MenuMealModel'}],
+        meal: [{type: ObjectId, ref: 'MenuMealModel'}],
         user: [{type: ObjectId, ref: 'usersModel'}],
     }
 );
-module.exports = mongoose.model('MenuMealModel', ratingMeal);
+module.exports = mongoose.model('ratingMeal', ratingMeal);
