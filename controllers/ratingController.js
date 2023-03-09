32345d421d233859b1b3f9ff97560f449a0e5921
gabriel dufresne <gabrielduf@hotmail.com>
2023-03-09 08:54:49 -0500

    modification de la fonction rating pour permettre d'update

--- controllers/ratingController.js
+++ controllers/ratingController.js
@@ -1,24 +1,31 @@
 const RatingMeal = require('../models/ratingModel');
+const Users = require("../models/usersModel");
 
 
 async function rateMeal(req, res){
-    console.log(req.body.meal)
-    try {
-        let rating = await RatingMeal.create({
-            MealRating: req.body.MealRating,
-            comment: req.body.comment,
-            meal: req.body.meal,
-            user: req.body.user,
-        });
-        console.log(rating)
-        await rating.save().then(() => {
-            return res.sendStatus(201);
-        })
-    }
-    catch(e) {
-        console.log(e);
-        return res.status(400).json({error: "error when creating rating"});
-    }
+        const sameRating = await RatingMeal.find({meal: req.body.meal}).exec();
+        if(sameRating.length > 0){
+            sameRating[0].MealRating = req.body.MealRating;
+            sameRating[0].comment = req.body.comment;
+
+            await sameRating[0].save().then(() => {
+                return res.sendStatus(201);
+            }).catch(err => {
+                return res.sendStatus(401);
+            });
+        }else{
+            let rating = await RatingMeal.create({
+                MealRating: req.body.MealRating,
+                comment: req.body.comment,
+                meal: req.body.meal,
+                user: req.body.user,
+            });
+            await rating.save().then(() => {
+                return res.sendStatus(201);
+            }).catch(err =>{
+                return res.sendStatus(401);
+            })
+        }
 }
 
 async function getHistory(req, res){
